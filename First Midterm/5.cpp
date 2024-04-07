1ba8c3868da95fef1ace5085773cda7292bb8cd6
Omer Tairi <omer.tairi@intelory.com>
2024-04-07 14:43:36 +0200

    all tests pass

--- First Midterm/5.cpp
+++ First Midterm/5.cpp
@@ -15,35 +15,35 @@ struct ComputerGame {
 
 void bestPlayer(ComputerGame *array, int n) {
   int maxPlayers = 0;
-  int bestPlayerIndex = -1;
-  int maxPoints = -1;
-  int maxLevel = 1;
-  int bestGameIndex;
+  int bestGameIndex = -1;
 
   for (int i = 0; i < n; i++) {
     if (array[i].numberOfPlayers > maxPlayers) {
       maxPlayers = array[i].numberOfPlayers;
       bestGameIndex = i;
-      bestPlayerIndex = -1; // reset
-    }
-    if (array[i].numberOfPlayers == maxPlayers) {
-      for (int j = 0; j < array[i].numberOfPlayers; j++) {
-        if (array[i].players[j].points > maxPoints ||
-            (array[i].players[j].points == maxPlayers &&
-             array[i].players[j].currentLevel > maxLevel)) {
-          maxPoints = array[i].players[j].points;
-          maxLevel = array[i].players[j].currentLevel;
-          bestPlayerIndex = j;
-          bestGameIndex = i;
-        }
-      }
     }
   }
 
-  if (bestPlayerIndex != -1) {
-    cout << "Najdobar igrac e igracot so korisnicko ime "
-         << array->players[bestPlayerIndex].name << " koj ja igra igrata "
-         << array[bestGameIndex].game << endl;
+  if (bestGameIndex != -1) {
+    int maxPoints = -1;
+    int maxLevel = 1;
+    int bestPlayerIndex = -1;
+
+    for (int j = 0; j < array[bestGameIndex].numberOfPlayers; j++) {
+      if (array[bestGameIndex].players[j].points > maxPoints ||
+          (array[bestGameIndex].players[j].points == maxPoints &&
+           array[bestGameIndex].players[j].currentLevel > maxLevel)) {
+        maxPoints = array[bestGameIndex].players[j].points;
+        maxLevel = array[bestGameIndex].players[j].currentLevel;
+        bestPlayerIndex = j;
+      }
+    }
+
+    if (bestPlayerIndex != -1) {
+      cout << "Najdobar igrac e igracot so korisnicko ime "
+           << array[bestGameIndex].players[bestPlayerIndex].name
+           << " koj ja igra igrata " << array[bestGameIndex].game << endl;
+    }
   }
 }
 
